1b59351dd10e3b6d3c1efd26d83e72e6a5af1af1
Martin Holzhauer <martin@holzhauer.eu>
2014-12-08 22:03:45 +0100

    console error handling

--- bootstrap.php
+++ bootstrap.php
@@ -4,16 +4,21 @@ date_default_timezone_set('UTC');
 \Symfony\Component\Debug\Debug::enable();
 
 $environment = getenv('ENVIRONMENT');
-$environment = $environment ?:\CBase\Ctream\Application\Application::ENV_DEVELOPMENT;
+$environment = $environment ?: \CBase\Ctream\Application\Application::ENV_DEVELOPMENT;
 
 $app = new \CBase\Ctream\Application\Application(
     [
-        'app.root_dir'      => __DIR__,
-        'app.name'          => 'ctream',
-        'app.environment'   => $environment,
+        'app.root_dir' => __DIR__,
+        'app.name' => 'ctream',
+        'app.environment' => $environment,
     ]
 );
 
-require $app->getConfigDir() . "/" . $environment . ".php";
+$environmentFile = $app->getConfigDir() . "/" . $environment . ".php";
+if (!file_exists($environmentFile)) {
+    throw new InvalidArgumentException(sprintf('Environment file for "%s" does not exists', $environment));
+}
+
+require $environmentFile;
 
 return $app;
--- web/index.php
+++ web/index.php
@@ -2,5 +2,4 @@
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = require dirname(__DIR__) . '/bootstrap.php';
-
 $app->run();
